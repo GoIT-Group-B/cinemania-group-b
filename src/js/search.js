e7f81a975e05260245a26be5754a68f9bc35d654
selinzi <selinbekar9@gmail.com>
2025-06-17 18:41:07 +0300

    update

---
+++ src/js/search.js
@@ -0,0 +1,29 @@
+const input = document.getElementById("search");
+const moviesContainer = document.getElementById("movies");
+ const clearButton = document.querySelector(".clear-button");
+// const messageBox = document.getElementById("message-box");
+
+
+function search() {
+    const value = input.value.trim();
+    moviesContainer.innerHTML = ""; // film alanları boş kalsın
+
+    if (value === "") {
+        alert("Warning! 'Please enter a search term.'");
+        return;
+    }
+    // showMessage("Searching, please wait...", "loading");
+    moviesContainer.scrollIntoView({ behavior: "smooth" });// Sayfayı filmler bölümüne kaydır
+}
+
+
+input.addEventListener("input", () => { /*çarpıyı input değeri varsa gösterir style.display görünürlük demek*/
+     clearButton.style.display = input.value ? "block" : "none";
+});
+function clearSearch() {
+    input.value = "";
+    clearButton.style.display = "none";
+    moviesContainer.innerHTML = "";
+    input.focus();
+
+}
