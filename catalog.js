e95550b88ad1815e65b3049ce4f1df9d3305abe9
furkancetin26 <32204507+furkancetin26@users.noreply.github.com>
2025-06-18 21:57:26 +0000

    Deploying to gh-pages from @ GoIT-Group-B/cinemania-group-b@d27c8860117a8c1da02c89e2487b93993a84fb98 🚀

--- catalog.js
+++ catalog.js
@@ -1,4 +1,4 @@
-import"./assets/theme-DTXbdikP.js";import{a as n}from"./assets/vendor-DDD7fsZd.js";const r="52238d7fab5c2c01b99e751619dd16ec",o="https://api.themoviedb.org/3",c="https://image.tmdb.org/t/p",i={POPULAR_MOVIES:"/movie/popular"};async function l(t,e,a={}){try{return(await n.get(`${t}${e}`,{params:{api_key:r,language:"en-US",page:1,...a}})).data}catch(s){throw console.error("Error fetching data:",s),s}}async function p(){const t=await l(o,i.POPULAR_MOVIES);console.log("response:",t.results);const e=await t.results;console.log("data:",e);const a=e[0];console.log("movie:",a),d(a)}function d(t){const e=document.querySelector(".hero-container");console.log(t),e.innerHTML=`
+import"./assets/theme-ZBcpXlQn.js";import{a as r}from"./assets/vendor-DDD7fsZd.js";const c="52238d7fab5c2c01b99e751619dd16ec",n="https://api.themoviedb.org/3",i="https://image.tmdb.org/t/p",o={POPULAR_MOVIES:"/movie/popular",IMG_W1280:"/w1280"};async function l(t,e,s={}){try{return(await r.get(`${t}${e}`,{params:{api_key:c,language:"en-US",page:1,...s}})).data}catch(a){throw console.error("Error fetching data:",a),a}}async function p(){const t=await l(n,o.POPULAR_MOVIES);console.log("response:",t.results);const e=await t.results;console.log("data:",e);const s=e[0];console.log("movie:",s),d(s)}function d(t){const e=document.querySelector(".hero-container");console.log(t);const s=`${i}${o.IMG_W1280}${t.backdrop_path}`;e.style.backgroundImage=`url('${s}')`,e.innerHTML=`
 <div class="hero-content">
   <h1 class="hero-title">${t.title}</h1>
   <div class="hero-rating">
@@ -10,10 +10,8 @@ import"./assets/theme-DTXbdikP.js";import{a as n}from"./assets/vendor-DDD7fsZd.j
     <button class="more-details-btn">More details</button>
   </div>
 </div>
-<div class="hero-image-container">
-  <img src="${c}/original${t.backdrop_path}" alt="${t.title} backdrop" class="hero-backdrop-image">
-</div>
-`}async function g(){const e=await(await fetch(`${o}/genre/movie/list?api_key=${API_KEY}&language=en-US`)).json();h(e.genres)}function h(t){const e=document.getElementById("category");e&&(e.innerHTML=t.map(a=>`
-        <div class="category-card">${a.name}</div>
+
+`}async function g(){const e=await(await fetch(`${n}/genre/movie/list?api_key=${API_KEY}&language=en-US`)).json();u(e.genres)}function u(t){const e=document.getElementById("category");e&&(e.innerHTML=t.map(s=>`
+        <div class="category-card">${s.name}</div>
       `).join(""))}p();g();
 //# sourceMappingURL=catalog.js.map
