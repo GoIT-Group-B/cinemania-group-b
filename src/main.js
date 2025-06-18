9f6b90e2ce5f592d4ccf70692f73d9361c29af9d
Furkan Çetin <furkan130526@gmail.com>
2025-06-18 19:14:40 +0300

    catalog-hero

--- src/main.js
+++ src/main.js
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-import { fetchTrendingMovie } from './js/hero.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-    
-    await fetchTrendingMovie();
-    await fetchCategories();
-});
-=======
 import './js/fetchMovies';
->>>>>>> 477952e02f62c29c0c1e315624011dc22592bbc0
