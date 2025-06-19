aa0480ad48ef6aa84af47b422f08bea2efba995b
furkancetin26 <32204507+furkancetin26@users.noreply.github.com>
2025-06-19 17:04:30 +0000

    Deploying to gh-pages from @ GoIT-Group-B/cinemania-group-b@92d43c30b165fc06bd1ed92248df16164499bf3f 🚀

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-import"./assets/theme-BnMLkBly.js";import{f as L,B as _,E as f,a as k,I as b}from"./assets/fetchMovies-BSsJlcs_.js";import"./assets/vendor-DDD7fsZd.js";const s=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
-<defs>
-<linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`,n=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="rgba(248, 65, 25, 1)" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
-<defs>
-<linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`,d=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_148_6991)" stroke-linejoin="round"/>
-<path d="M9 1.6875V12.7969L4.14844 16.3125L6.04688 10.6875L1.125 7.3125H7.17188L9 1.6875Z" fill="url(#paint1_linear_148_6991)"/>
-<defs>
-<linearGradient id="paint0_linear_148_6991" x1="3.04877" y1="2.73251" x2="13.478" y2="16.7124" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-<linearGradient id="paint1_linear_148_6991" x1="2.08688" y1="2.73251" x2="12.1506" y2="9.47748" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`,w=document.querySelector(".home-hero");async function m(){try{const{results:t}=await L(_,f.TRENDING_DAY,{page:1}),e=t[Math.floor(Math.random()*t.length)];y(e),document.getElementById("trailer-btn").addEventListener("click",()=>{alert("Sorry! We couldn’t find the trailer.")}),document.getElementById("details-btn").addEventListener("click",()=>{alert("Movie details will be shown here.")})}catch(t){console.error("Trend film alınamadı:",t),x()}}function x(){const t=`
+import"./assets/theme-BdXRRVLI.js";import{f as c,B as p,E as l,c as u,a as m,I as b}from"./assets/stars-ZdAbMgea.js";import"./assets/vendor-DDD7fsZd.js";const g=document.querySelector(".home-hero");async function v(){try{const{results:e}=await c(p,l.TRENDING_DAY,{page:1}),t=e[Math.floor(Math.random()*e.length)];k(t),document.getElementById("trailer-btn").addEventListener("click",()=>{alert("Sorry! We couldn’t find the trailer.")}),document.getElementById("details-btn").addEventListener("click",()=>{alert("Movie details will be shown here.")})}catch(e){console.error("Trend film alınamadı:",e),f()}}function f(){const e=`
     <div class="hero-wrap">
       <div class="thumb">
         <picture class="background-image">
@@ -46,58 +17,29 @@ import"./assets/theme-BnMLkBly.js";import{f as L,B as _,E as f,a as k,I as b}fro
         </div>
       </div>
     </div>
-  `;w.innerHTML=t;const e=document.querySelector(".getstarted-btn");e&&e.addEventListener("click",()=>{location.href="catalog.html"})}function y(t){let e="";switch(Math.round(t.vote_average)){case 0:e=s.repeat(5);break;case 1:e=d+s.repeat(4);break;case 2:e=n+s.repeat(4);break;case 3:e=n+d+s.repeat(3);break;case 4:e=n.repeat(2)+s.repeat(3);break;case 5:e=n.repeat(2)+d+s.repeat(2);break;case 6:e=n.repeat(3)+s.repeat(2);break;case 7:e=n.repeat(3)+d+s;break;case 8:e=n.repeat(4)+s;break;case 9:e=n.repeat(4)+d;break;case 10:e=n.repeat(5);break;default:e="N/A"}const r=`
+  `;g.innerHTML=e;const t=document.querySelector(".getstarted-btn");t&&t.addEventListener("click",()=>{location.href="catalog.html"})}function k(e){let t=u(e.vote_average);const a=`
     <div class="hero-wrap">
       <div class="thumb">
         <div class="background-image">
-          <img src="https://image.tmdb.org/t/p/original${t.backdrop_path}" alt="Hero image" class="backend" loading="lazy" />
+          <img src="https://image.tmdb.org/t/p/original${e.backdrop_path}" alt="Hero image" class="backend" loading="lazy" />
         </div>
         <div class="hero-wrap__content">
-          <h1 class="title">${t.title||t.name}</h1>
-          <div class='start-rate__hero'>${e}</div> 
-          <p class="description">${t.overview}</p>
+          <h1 class="title">${e.title||e.name}</h1>
+          <div class='start-rate__hero'>${t}</div> 
+          <p class="description">${e.overview}</p>
           <button class="watch-trailer__btn" id="trailer-btn">Watch trailer</button>
           <button class="more-details__btn" id="details-btn">More details</button>
         </div>
       </div>
     </div>
-  `;w.innerHTML=r}m();const a=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
-<defs>
-<linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`,i=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="rgba(248, 65, 25, 1)" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
-<defs>
-<linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`,c=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_148_6991)" stroke-linejoin="round"/>
-<path d="M9 1.6875V12.7969L4.14844 16.3125L6.04688 10.6875L1.125 7.3125H7.17188L9 1.6875Z" fill="url(#paint1_linear_148_6991)"/>
-<defs>
-<linearGradient id="paint0_linear_148_6991" x1="3.04877" y1="2.73251" x2="13.478" y2="16.7124" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-<linearGradient id="paint1_linear_148_6991" x1="2.08688" y1="2.73251" x2="12.1506" y2="9.47748" gradientUnits="userSpaceOnUse">
-<stop stop-color="#F84119"/>
-<stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
-</linearGradient>
-</defs>
-</svg>`;function $(t){let e="";if(!t)return e=`${a.repeat(5)}`,`<div>${e}</div>`;switch(Math.round(t)){case 0:e=`${a.repeat(5)}`;break;case 1:e=`${c}${a.repeat(4)}`;break;case 2:e=`${i}${a.repeat(4)}`;break;case 3:e=`${i}${c}${a.repeat(3)}`;break;case 4:e=`${i.repeat(2)}${a.repeat(3)}`;break;case 5:e=`${i.repeat(2)}${c}${a.repeat(2)}`;break;case 6:e=`${i.repeat(3)}${a.repeat(2)}`;break;case 7:e=`${i.repeat(3)}${c}${a}`;break;case 8:e=`${i.repeat(4)}${a}`;break;case 9:e=`${i.repeat(4)}${c}`;break;case 10:e=`${i.repeat(5)}`;break;default:throw new Error("Invalid rating")}return`<div>${e}</div>`}let u=!1,p=[],v={};async function M(){const t=document.getElementById("trends-container"),e=document.getElementById("see-all-trends");v=await k(),p=(await L(_,f.TRENDING_WEEK)).results;const r=window.innerWidth<768?1:3;g(t,p.slice(0,r)),e.addEventListener("click",()=>{if(!t.classList.contains("is-expanded"))g(t,p),t.classList.add("is-expanded"),e.textContent="Show Less",u=!0;else{const h=window.innerWidth<768?1:3;g(t,p.slice(0,h)),t.classList.remove("is-expanded"),e.textContent="See All",u=!1}}),window.addEventListener("resize",()=>{if(!u){const l=window.innerWidth<768?1:3;g(t,p.slice(0,l))}})}function g(t,e){t.innerHTML=e.map((o,r)=>{const l=r>=3?(r-2)*100:0;return S(o,l)}).join("")}function S(t,e=0){var l;const o=t.genre_ids.map(h=>v[h]).join(", "),r=$(t.vote_average);return`
-    <div class="trend-card" data-id="${t.id}" style="animation-delay: ${e}ms;">
-      <img src="${b}${f.IMG_W500}${t.poster_path}" alt="${t.title}">
+  `;g.innerHTML=a}v();let d=!1,r=[],h={};async function w(){const e=document.getElementById("trends-container"),t=document.getElementById("see-all-trends");h=await m(),r=(await c(p,l.TRENDING_WEEK)).results;const s=window.innerWidth<768?1:3;i(e,r.slice(0,s)),t.addEventListener("click",()=>{if(!e.classList.contains("is-expanded"))i(e,r),e.classList.add("is-expanded"),t.textContent="Show Less",d=!0;else{const o=window.innerWidth<768?1:3;i(e,r.slice(0,o)),e.classList.remove("is-expanded"),t.textContent="See All",d=!1}}),window.addEventListener("resize",()=>{if(!d){const n=window.innerWidth<768?1:3;i(e,r.slice(0,n))}})}function i(e,t){e.innerHTML=t.map((a,s)=>{const n=s>=3?(s-2)*100:0;return x(a,n)}).join("")}function x(e,t=0){var n;const a=e.genre_ids.map(o=>h[o]).join(", "),s=u(e.vote_average);return`
+    <div class="trend-card" data-id="${e.id}" style="animation-delay: ${t}ms;">
+      <img src="${b}${l.IMG_W500}${e.poster_path}" alt="${e.title}">
       <div class="trend-info">
-        <h3>${t.title}</h3>
-        <p>${o} | ${((l=t.release_date)==null?void 0:l.split("-")[0])||"N/A"}</p>
-        <div class="trend-stars">${r}</div>
+        <h3>${e.title}</h3>
+        <p>${a} | ${((n=e.release_date)==null?void 0:n.split("-")[0])||"N/A"}</p>
+        <div class="trend-stars">${s}</div>
       </div>
     </div>
-  `}M();
+  `}w();
 //# sourceMappingURL=index.js.map
