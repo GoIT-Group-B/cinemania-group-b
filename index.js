692a475ea4d65d230eef1e83419b403fa2d0d214
gozdearici <gozdenurarici@gmail.com>
2025-06-19 14:34:04 +0000

    Deploying to gh-pages from @ GoIT-Group-B/cinemania-group-b@dcb9c4b0411f1d7619a4ae2c63838d4097b72169 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import"./assets/theme-CnDw7Ayj.js";import{f,a as g,E as L,B as _,I as w}from"./assets/fetchMovies-DXPj_YoO.js";import"./assets/vendor-DDD7fsZd.js";const a=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+import"./assets/theme-C3VdtH2B.js";import{f,a as g,E as L,B as _,I as w}from"./assets/fetchMovies-DXPj_YoO.js";import"./assets/vendor-DDD7fsZd.js";const a=`<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
 <defs>
 <linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
